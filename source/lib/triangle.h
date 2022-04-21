c756e541bf3b44de0159766702c9a21b2fa1afc7
Mihail Vlasyuk <mihailvlasyuk@MacBook-Pro-Mihail.local>
2022-04-21 17:28:17 +0700

    edit triangle.h

--- source/lib/triangle.h
+++ source/lib/triangle.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "point.h"
 
+
 float calculateSide(struct Point point1, struct Point point2);
 float calculateTrianglePerimeter(struct Point points[4]);
 float calculateTriangleArea(struct Point points[4]);
