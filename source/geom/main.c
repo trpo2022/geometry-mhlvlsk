a629f3e716d65b041cd8b43ed4a98b3b2b559634
Mihail Vlasyuk <mihailvlasyuk@MacBook-Pro-Mihail.local>
2022-04-28 15:55:23 +0700

    clang-format main.c

--- source/geom/main.c
+++ source/geom/main.c
@@ -1,64 +1,60 @@
 #include "inputread.h"
-#include "lib/triangle.h"
 #include "lib/circle.h"
-#include "lib/point.h"
 #include "lib/object.h"
+#include "lib/point.h"
+#include "lib/triangle.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void getObj(int i, object* obj)
-{
-    char* input = malloc(sizeof(char) * 32);
-    char* token = NULL;
-    initInput(input);
-    char* delims = "( ,)";
-    switch (check_object(input, &token)) {
-    case 0:
-        printf("\n%d. %s:\n", i, token);
-        struct Circle circle;
-        circle.center.x = atof(strtok(NULL, delims));
-        circle.center.y = atof(strtok(NULL, delims));
-        circle.radius = atof(strtok(NULL, delims));
+void getObj(int i, object *obj) {
+  char *input = malloc(sizeof(char) * 32);
+  char *token = NULL;
+  initInput(input);
+  char *delims = "( ,)";
+  switch (check_object(input, &token)) {
+  case 0:
+    printf("\n%d. %s:\n", i, token);
+    struct Circle circle;
+    circle.center.x = atof(strtok(NULL, delims));
+    circle.center.y = atof(strtok(NULL, delims));
+    circle.radius = atof(strtok(NULL, delims));
 
-        obj->circle = circle;
-        obj->type = CIRCLE;
-        printCircleInfo(circle);
-        break;
+    obj->circle = circle;
+    obj->type = CIRCLE;
+    printCircleInfo(circle);
+    break;
 
-    case 1:
-        printf("\n%d. %s:\n", i, token);
-        struct Triangle triangle;
+  case 1:
+    printf("\n%d. %s:\n", i, token);
+    struct Triangle triangle;
 
-        for (int i = 0; i < 4; i++) {
-            triangle.points[i].x = atof(strtok(NULL, delims));
-            triangle.points[i].y = atof(strtok(NULL, delims));
-        }
-
-        if (triangle.points[3].x != triangle.points[0].x
-            || triangle.points[3].y != triangle.points[0].y) {
-            printf("Error with handling the first/last point of the "
-                   "triangle.\nCheck if your data is correct.\n");
-        }
-
-        obj->triangle = triangle;
-        obj->type = TRIANGLE;
-        printTriangleInfo(triangle);
-        break;
-
-    default:
-        printf("Incorrect input.\n");
+    for (int i = 0; i < 4; i++) {
+      triangle.points[i].x = atof(strtok(NULL, delims));
+      triangle.points[i].y = atof(strtok(NULL, delims));
     }
-    free(input);
+
+    if (triangle.points[3].x != triangle.points[0].x ||
+        triangle.points[3].y != triangle.points[0].y) {
+      printf("Error with handling the first/last point of the "
+             "triangle.\nCheck if your data is correct.\n");
+    }
+
+    obj->triangle = triangle;
+    obj->type = TRIANGLE;
+    printTriangleInfo(triangle);
+    break;
+
+  default:
+    printf("Incorrect input.\n");
+  }
+  free(input);
 }
 
-int main()
-{
-    object objects[3];
-    for (int i = 0; i < 3; i++) {
-        getObj(i + 1, &objects[i]);
-    }
-    return 0;
+int main() {
+  object objects[3];
+  for (int i = 0; i < 3; i++) {
+    getObj(i + 1, &objects[i]);
+  }
+  return 0;
 }
-
-
