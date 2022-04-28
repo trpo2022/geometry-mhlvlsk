667837e6e856b830a5e149d4612a6e69f7288723
Mihail Vlasyuk <mihailvlasyuk@MacBook-Pro-Mihail.local>
2022-04-28 19:58:37 +0700

    use clang-format

--- source/geom/main.c
+++ source/geom/main.c
@@ -7,54 +7,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-void getObj(int i, object *obj) {
-  char *input = malloc(sizeof(char) * 32);
-  char *token = NULL;
-  initInput(input);
-  char *delims = "( ,)";
-  switch (check_object(input, &token)) {
-  case 0:
-    printf("\n%d. %s:\n", i, token);
-    struct Circle circle;
-    circle.center.x = atof(strtok(NULL, delims));
-    circle.center.y = atof(strtok(NULL, delims));
-    circle.radius = atof(strtok(NULL, delims));
+void getObj(int i, object* obj)
+{
+    char* input = malloc(sizeof(char) * 32);
+    char* token = NULL;
+    initInput(input);
+    char* delims = "( ,)";
+    switch (check_object(input, &token)) {
+    case 0:
+        printf("\n%d. %s:\n", i, token);
+        struct Circle circle;
+        circle.center.x = atof(strtok(NULL, delims));
+        circle.center.y = atof(strtok(NULL, delims));
+        circle.radius = atof(strtok(NULL, delims));
 
-    obj->circle = circle;
-    obj->type = CIRCLE;
-    printCircleInfo(circle);
-    break;
+        obj->circle = circle;
+        obj->type = CIRCLE;
+        printCircleInfo(circle);
+        break;
 
-  case 1:
-    printf("\n%d. %s:\n", i, token);
-    struct Triangle triangle;
+    case 1:
+        printf("\n%d. %s:\n", i, token);
+        struct Triangle triangle;
 
-    for (int i = 0; i < 4; i++) {
-      triangle.points[i].x = atof(strtok(NULL, delims));
-      triangle.points[i].y = atof(strtok(NULL, delims));
+        for (int i = 0; i < 4; i++) {
+            triangle.points[i].x = atof(strtok(NULL, delims));
+            triangle.points[i].y = atof(strtok(NULL, delims));
+        }
+
+        if (triangle.points[3].x != triangle.points[0].x
+            || triangle.points[3].y != triangle.points[0].y) {
+            printf("Ошибка обработки крайних точек в треугольнике.\nПроверьте "
+                   "введенные данные.\n");
+        }
+
+        obj->triangle = triangle;
+        obj->type = TRIANGLE;
+        printTriangleInfo(triangle);
+        break;
+
+    default:
+        printf("Введены неверные данные.\n");
     }
-
-    if (triangle.points[3].x != triangle.points[0].x ||
-        triangle.points[3].y != triangle.points[0].y) {
-      printf("Ошибка обработки крайних точек в треугольнике.\nПроверьте "
-             "введенные данные.\n");
-    }
-
-    obj->triangle = triangle;
-    obj->type = TRIANGLE;
-    printTriangleInfo(triangle);
-    break;
-
-  default:
-    printf("Введены неверные данные.\n");
-  }
-  free(input);
+    free(input);
 }
 
-int main() {
-  object objects[3];
-  for (int i = 0; i < 3; i++) {
-    getObj(i + 1, &objects[i]);
-  }
-  return 0;
+int main()
+{
+    object objects[3];
+    for (int i = 0; i < 3; i++) {
+        getObj(i + 1, &objects[i]);
+    }
+    return 0;
 }
