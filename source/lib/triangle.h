479fee3c5804e4bf6bbd02ee803aeb6bda9ff833
Mihail Vlasyuk <mihailvlasyuk@MacBook-Pro-Mihail.local>
2022-04-28 15:41:48 +0700

    add struct Triangle

--- source/lib/triangle.h
+++ source/lib/triangle.h
@@ -2,8 +2,12 @@
 #include "point.h"
 
 
+struct Triangle {
+    struct Point points[4];
+};
+
 float calculateSide(struct Point point1, struct Point point2);
 float calculateTrianglePerimeter(struct Point points[4]);
 float calculateTriangleArea(struct Point points[4]);
-void printTriangleInfo(struct Point points[4], float perimeter, float area);
+void printTriangleInfo(struct Triangle triangle);
 
